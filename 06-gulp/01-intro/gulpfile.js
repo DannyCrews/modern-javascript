317b327f4094ba90d75abca15efd38e46fb77b3a
dannycrews <dancrews@me.com>
2016-08-06 18:00:44 -0400

    Add babel. First task

---
+++ 06-gulp/01-intro/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require("gulp");
+babel = require("gulp-babel");
+
+gulp.task("default", () => {
+        return gulp
+        // .src(["./src/scripts/application.js", "./src/scripts/utility.js"])
+        .src("./src/scripts/**/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("./public/scripts"));
+});
