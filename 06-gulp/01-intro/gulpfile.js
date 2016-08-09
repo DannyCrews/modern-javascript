c614d0915ac24fe1898c463e859d7b35ae811086
dannycrews <dancrews@me.com>
2016-08-09 18:55:12 -0400

    set up gulp dependencies

--- 06-gulp/01-intro/gulpfile.js
+++ 06-gulp/01-intro/gulpfile.js
@@ -1,10 +1,44 @@
-var gulp = require("gulp");
-babel = require("gulp-babel");
+var gulp = require("gulp"),
+    babel = require("gulp-babel"),
+    less = require("gulp-less");
 
-gulp.task("default", () => {
-        return gulp
+gulp.task("dev:styles", devStyles);
+gulp.task("dev:scripts", devScripts);
+gulp.task(clean);
+
+gulp.task("default",
+    gulp.series(
+        clean,
+        gulp.parallel(
+            devStyles,
+            devScripts),
+        publish));
+
+function devStyles() {
+    return gulp
+        .src("./src/styles/site.less")
+        .pipe(less())
+        .pipe(gulp.dest("./public/styles"));
+}
+
+function devScripts() {
+    return gulp
         // .src(["./src/scripts/application.js", "./src/scripts/utility.js"])
         .src("./src/scripts/**/*.js")
         .pipe(babel())
         .pipe(gulp.dest("./public/scripts"));
-});
+}
+
+function clean(c) {
+    setTimeout(() => {
+        console.log("CLEANED");
+        c();
+    }, 1000);
+}
+
+function publish(c) {
+    setTimeout(() => {
+        console.log("PUBLISHED");
+        c();
+    }, 1000);
+}
